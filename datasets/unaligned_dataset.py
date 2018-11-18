358fc1a033f06484173808ff55dbc62801416056
pomelyu <pomelyu5199@icloud.com>
2018-11-18 11:21:32 +0800

    :card_file_box: implement unaligned dataset

---
+++ datasets/unaligned_dataset.py
@@ -0,0 +1,47 @@
+import random
+from PIL import Image
+from torchvision import transforms as T
+from .base_dataset import BaseDataset
+from .image_folder import make_dataset
+
+class UnalignedDataset(BaseDataset):
+    def name(self):
+        return "UnalignedDataset"
+
+    @staticmethod
+    def modify_commandline_options(parser, is_train):
+        parser.add_argument("--dataA", type=str, required=True)
+        parser.add_argument("--dataB", type=str, required=True)
+        return parser
+
+    def initialize(self, opt):
+        self.opt = opt
+        self.A_paths = sorted(make_dataset(opt.dataA))
+        self.B_paths = sorted(make_dataset(opt.dataB))
+        self.A_size = len(self.A_paths)
+        self.B_size = len(self.B_paths)
+        self.transforms = T.Compose([
+            T.Resize(opt.load_size),
+            T.CenterCrop(opt.crop_size),
+            T.ToTensor(),
+            T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ])
+
+
+    def __getitem__(self, index):
+        A_path = self.A_paths[index % self.A_size]
+        if self.opt.serial_batches:
+            B_path = self.B_paths[index % self.B_size]
+        else:
+            B_path = self.B_paths[random.randint(0, self.B_size-1)]
+
+        A_image = Image.open(A_path)
+        B_image = Image.open(B_path)
+
+        A = self.transforms(A_image)
+        B = self.transforms(B_image)
+
+        return {"A": A, "B": B, "A_paths": A_path, "B_paths": B_path}
+
+    def __len__(self):
+        return max(self.A_size, self.B_size)
---
+++ tests/test_unaligned_dataset.py
@@ -0,0 +1,34 @@
+import torch
+import context # pylint: disable=unused-import
+from datasets import UnalignedDataset
+
+class Opt():
+    dataA = "data/demo/faces"
+    dataB = "data/demo/faces"
+    load_size = 64
+    crop_size = 64
+    isTrain = True
+
+def test_unaligned_dataset():
+    demo_opt = Opt()
+    demo_dataset = UnalignedDataset()
+    demo_dataset.initialize(demo_opt)
+
+    res = demo_dataset[0]
+    demo_A = res["A"]
+    demo_B = res["B"]
+    assert torch.is_tensor(demo_A)
+    assert demo_A.shape == torch.Tensor(3, demo_opt.crop_size, demo_opt.crop_size).shape
+    assert demo_A.min() >= -1 and demo_A.max() <= 1
+    assert isinstance(demo_A, torch.FloatTensor)
+
+    assert torch.is_tensor(demo_B)
+    assert demo_B.shape == torch.Tensor(3, demo_opt.crop_size, demo_opt.crop_size).shape
+    assert demo_B.min() >= -1 and demo_B.max() <= 1
+    assert isinstance(demo_B, torch.FloatTensor)
+
+    assert isinstance(res["A_paths"], str)
+    assert isinstance(res["B_paths"], str)
+
+if __name__ == "__main__":
+    test_unaligned_dataset()
