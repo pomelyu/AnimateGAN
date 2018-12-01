:bug: fix gradient reversed layer(nn.module do not has backward method)

models/XGAN.py
@@ -18,6 +18,7 @@ class XGAN(BaseModel):
         parser.add_argument("--lambda_dann", type=float, default=1.0)
         parser.add_argument("--lambda_sem", type=float, default=1.0)
         parser.add_argument("--lambda_rec", type=float, default=1.0)
+        parser.add_argument("--revsersed_ratio", type=float, default=1.0)
         return parser
 
     def initialize(self, opt):
@@ -44,7 +45,7 @@ class XGAN(BaseModel):
         if opt.isTrain:
             # self.netD_A = XGAN_Discriminator()
             self.netD_B = XGAN_Discriminator()
-            self.netLC = XGAN_LatentClassifer()
+            self.netLC = XGAN_LatentClassifer(opt.revsersed_ratio)
             self.optimizer_G = torch.optim.Adam(itertools.chain(
                 self.netEn_A.parameters(),
                 self.netEn_B.parameters(),
@@ -225,10 +226,10 @@ class XGAN_Discriminator(nn.Module):
 
 
 class XGAN_LatentClassifer(nn.Module):
-    def __init__(self):
+    def __init__(self, revsersed_ratio=1):
         super(XGAN_LatentClassifer, self).__init__()
         self.model = nn.Sequential(
-            GradientReverseLayer(1),
+            GradientReverseLayer(revsersed_ratio=revsersed_ratio),
             nn.Linear(1024, 256),
             nn.BatchNorm1d(256),
             nn.ReLU(True),

models/building_blocks/layers.py
@@ -1,4 +1,5 @@
 import functools
+import torch
 from torch import nn
 import torch.nn.functional as F
 
@@ -68,12 +69,20 @@ class L2NormalizeLayer(nn.Module):
         return nn.functional.normalize(x, p=2, dim=1)
 
 class GradientReverseLayer(nn.Module):
-    def __init__(self, scale):
+    def __init__(self, revsersed_ratio=1):
         super(GradientReverseLayer, self).__init__()
-        self.scale = scale
+        self.layer = GradientReverse(revsersed_ratio)
 
     def forward(self, x):
-        return x.clone()
+        return self.layer(x)
+
+class GradientReverse(torch.autograd.Function):
+    def __init__(self, revsersed_ratio=1):
+        super(GradientReverse, self).__init__()
+        self.revsersed_ratio = revsersed_ratio
+
+    def forward(self, x):
+        return x.view_as(x)
 
     def backward(self, grad_out):
-        return -self.scale * grad_out.clone()
+        return -self.revsersed_ratio * grad_out.clone()