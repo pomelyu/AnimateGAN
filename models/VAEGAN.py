dfc53f2c0e28ef2ff0bd198152711bae259b9d59
pomelyu <pomelyu5199@icloud.com>
2018-12-09 10:28:54 +0800

    :alembic: add WGANGP to VAEGAN model to prevent model collapse

--- models/VAEGAN.py
+++ models/VAEGAN.py
@@ -3,7 +3,7 @@ from torch import nn
 from .base_model import BaseModel
 from .VAE import Encoder, Decoder, KLLoss
 from .DCGAN import DCGAN_D
-from .building_blocks.loss import GANLoss
+from .building_blocks.loss import WGANGPLoss
 from .util import init_net
 
 # pylint: disable=attribute-defined-outside-init
@@ -16,10 +16,13 @@ class VAEGAN(BaseModel):
     def modify_commandline_options(parser, is_train=True):
         parser.set_defaults(ngf=32)
         parser.set_defaults(latent_size=256)
+        parser.set_defaults(beta1=0)
         if is_train:
             # weight calculated by observered variance
             parser.add_argument("--lambda_kl", type=float, default=1.0)
             parser.add_argument("--lambda_idt", type=float, default=1.0)
+            parser.add_argument("--lambda_gp", type=float, default=10.0)
+            parser.add_argument("--every_g", type=int, default=5)
         return parser
 
     @staticmethod
@@ -32,13 +35,14 @@ class VAEGAN(BaseModel):
         BaseModel.initialize(self, opt)
 
         self.opt = opt
+        self.niters = 0
         self.loss_names = ["KL", "Idt", "G", "D"]
         self.model_names = ["Encoder", "G", "D"]
         self.visual_names = ["real", "fake", "fake_p"]
 
         self.netEncoder = Encoder(opt.latent_size, ngf=opt.ngf)
         self.netG = Decoder(opt.latent_size, ngf=opt.ngf)
-        self.netD = DCGAN_D(3, 1, ndf=opt.ndf, norm="batch", use_bias=False)
+        self.netD = DCGAN_D(3, 1, ndf=opt.ndf, norm="none", use_bias=False)
 
         self.netEncoder = init_net(self.netEncoder, opt.init_type, opt.init_gain, opt.gpu_ids)
         self.netG = init_net(self.netG, opt.init_type, opt.init_gain, opt.gpu_ids)
@@ -47,7 +51,7 @@ class VAEGAN(BaseModel):
         if self.isTrain:
             self.criterionIdt = nn.MSELoss()
             self.criterionKL = KLLoss()
-            self.criterionGAN = GANLoss(use_lsgan=False).to(self.device)
+            self.criterionGAN = WGANGPLoss(opt.lambda_gp, self.device).to(self.device)
             self.optimizerEn = torch.optim.Adam(self.netEncoder.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
             self.optimizerG = torch.optim.Adam(self.netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
             self.optimizerD = torch.optim.Adam(self.netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
@@ -73,7 +77,10 @@ class VAEGAN(BaseModel):
         fake_score = self.netD(self.fake)
         fake_score_p = self.netD(self.fake_p)
 
-        self.loss_G = 0.5 * (self.criterionGAN(fake_score, True) + self.criterionGAN(fake_score_p, True))
+        self.loss_G = 0.5 * (
+            self.criterionGAN.loss_G(fake_score) + \
+            self.criterionGAN.loss_G(fake_score_p)
+        )
         loss_idt = self.criterionIdt(self.fake, self.real) * self.opt.lambda_idt
         loss = self.loss_G + loss_idt
         loss.backward()
@@ -82,30 +89,35 @@ class VAEGAN(BaseModel):
         mu, logvar = self.netEncoder(self.real)
         z = self.reparameterize(mu, logvar)
         z_p = torch.randn_like(mu)
+        fake = self.netG(z)
+        fake_p = self.netG(z_p)
         real_score = self.netD(self.real)
-        fake_score = self.netD(self.netG(z))
-        fake_score_p = self.netD(self.netG(z_p))
+        interp = self.criterionGAN.interp_real_fake(self.real, fake)
+        interp_p = self.criterionGAN.interp_real_fake(self.real, fake_p)
 
-        self.loss_D = (
-            self.criterionGAN(real_score, True) + \
-            self.criterionGAN(fake_score, False) + \
-            self.criterionGAN(fake_score_p, False)
-        ) / 3
+        self.loss_D = 0.5 * (
+            self.criterionGAN.loss_D(real_score, self.netD(fake), self.netD(interp), interp) + \
+            self.criterionGAN.loss_D(real_score, self.netD(fake_p), self.netD(interp_p), interp_p)
+        )
         self.loss_D.backward()
 
     def optimize_parameters(self):
         self.forward()
 
-        self.set_requires_grad([self.netD], False)
-        self.optimizerEn.zero_grad()
-        self.backward_En()
-        self.optimizerEn.step()
+        if self.niters % self.opt.every_g == 0:
+            self.set_requires_grad([self.netD], False)
+            self.optimizerEn.zero_grad()
+            self.backward_En()
+            self.optimizerEn.step()
 
-        self.optimizerG.zero_grad()
-        self.backward_G()
-        self.optimizerG.step()
+            self.optimizerG.zero_grad()
+            self.backward_G()
+            self.optimizerG.step()
+            self.niters = 0
 
         self.set_requires_grad([self.netD], True)
         self.optimizerD.zero_grad()
         self.backward_D()
         self.optimizerD.step()
+
+        self.niters += 1
